971933be558d8a1d57068ae9740f20ba1afefc0a
Eduardo Thomas <eduardothomas64@gmail.com>
2024-06-03 04:58:53 -0500

    Update ContentView.swift

--- visionOS-solar-system/ContentView.swift
+++ visionOS-solar-system/ContentView.swift
@@ -10,12 +10,17 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
+    @State private var messageString = "Hello World!"
     var body: some View {
         VStack {
             Model3D(named: "Scene", bundle: realityKitContentBundle)
                 .padding(.bottom, 50)
 
-            Text("Hello, world!")
+            Text(messageString)
+            Button("Click") {
+                print("asdasd")
+                messageString = "asdasdsad"
+            }
         }
         .padding()
     }
