8394fea39cc2f4e2ca61763301f39e2e71c40568
Eduardo Thomas <eduardothomas64@gmail.com>
2024-06-03 04:46:29 -0500

    Initial Commit

---
+++ visionOS-solar-system/visionOS_solar_systemApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionOS_solar_systemApp.swift
+//  visionOS-solar-system
+//
+//  Created by Eduardo Thomas on 06/03/24.
+//
+
+import SwiftUI
+
+@main
+struct visionOS_solar_systemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
